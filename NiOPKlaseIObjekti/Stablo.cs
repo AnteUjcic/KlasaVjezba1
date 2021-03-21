d982cc1aaf0ff49acfb96f5e6c10c0efc994fdbe
AnteUjcic <58471899+AnteUjcic@users.noreply.github.com>
2021-03-21 19:18:26 +0100

    Add files via upload

---
+++ NiOPKlaseIObjekti/Stablo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiOPKlaseIObjekti
+{
+    class Stablo
+    {
+        bool opadajuListovi;
+
+        public Stablo(bool opadajuListovi)
+        {
+            this.opadajuListovi = opadajuListovi;
+        }
+
+        public bool GetSetListoviToPadajuOrNePadaju { get; set; }
+
+
+    }
+}
